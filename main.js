4ff91cea463e4ae08cd5f985a8148a2b609c6c40
A-Jatav <108002072+A-Jatav@users.noreply.github.com>
2023-01-23 19:32:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 function preload(){
+    moustache = loadImage('https://i.postimg.cc/fR4gJm4Z/moustache-removebg-preview.png');
 };
 
 function setup(){
@@ -7,6 +8,28 @@ function setup(){
     video = createCapture(VIDEO);
     video.size(400, 400);
     video.hide();
+    posenet = ml5.poseNet(video, modelLoaded);
+    posenet.on('pose', gotPoses);
+};
+
+var nose_x = 0;
+var nose_y = 0;
+
+function gotPoses(results){
+    if(results.length > 0){
+        nose_x = results[0].pose.nose.x;
+        nose_y = results[0].pose.nose.y;
+        console.log("Nose X = "+nose_x+", Nose Y = "+nose_y);
+    }
+};
+
+function draw(){
+    image(video, 0, 0, 400, 400);
+    image(moustache, nose_x - 20, nose_y, 50, 50);
+};
+
+function modelLoaded(){
+    console.log("Posenet has been initializedd correctly!");
 };
 
 function take_snapshot(){
